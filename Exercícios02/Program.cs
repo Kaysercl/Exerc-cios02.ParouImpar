b1e16963cb87d967018a5994aedffc968aa1c9bd
Kayser <Kayser@DESKTOP-JD3R4EO>
2022-02-08 14:59:37 -0300

    Add project files.

---
+++ Exercícios02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercícios02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+
+            Console.WriteLine("ESCREVA 1 NUMERO PARA SABER SE É PAR OU IMPAR");
+            X = int.Parse(Console.ReadLine());
+
+            if (X % 2 == 0)
+            {
+                Console.WriteLine("SEU NUMERO É PAR");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("SEU NUMERO É IMPAR");
+                Console.ReadLine();
+            }
+            
+            Console.Clear();            
+        }
+    }
+}
